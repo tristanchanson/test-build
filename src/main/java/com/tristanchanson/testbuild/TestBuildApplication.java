7bcd4db0195182c94ec55161261f77e5a3752429
Tristan Hanson <tristanchanson@gmail.com>
2019-02-09 19:46:38 -0600

    Initial Commit

---
+++ src/main/java/com/tristanchanson/testbuild/TestBuildApplication.java
@@ -0,0 +1,14 @@
+package com.tristanchanson.testbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBuildApplication.class, args);
+	}
+
+}
+
